814dc7a06ff0ffd6eab5750865adba87b98b68a5
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2021-10-27 21:59:57 -0400

    chore: config files

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    parserOptions: {
+        "project": "./tsconfig.json",
+        "tsconfigRootDir": __dirname,
+        "sourceType": "module"
+    },
+    rules: {
+      "no-console": "off",
+      "no-case-declarations": "off",
+      "no-await-in-loop": "off",
+      "import/prefer-default-export": "off",
+    },
+    root: true,
+    parser: '@typescript-eslint/parser',
+    plugins: [
+      '@typescript-eslint',
+    ],
+    extends: "airbnb-base-typescript-prettier"
+  };
+  
