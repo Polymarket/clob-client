9d7b336cca9c717a2de7f7746104f332606637ce
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2021-10-29 19:08:33 -0400

    fix: warning copy change

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const CREDS_CREATION_WARNING = `🚨🚨🚨
-Your credentials CANNOT be recovered once created. 
+Your credentials CANNOT be recovered after they've been created. 
 Be sure to store them safely!
 🚨🚨🚨`;
