92e26f20a600d8132ae066a8c02b9a3525bef666
Rodrigo <95635797+poly-rodr@users.noreply.github.com>
2022-05-31 18:07:46 -0300

    Side is part of the response

--- examples/getLastTradePrice.ts
+++ examples/getLastTradePrice.ts
@@ -15,7 +15,7 @@ async function main() {
     const clobClient = new ClobClient(host, wallet);
 
     console.log(`Price: `);
-    const resp = await clobClient.getLastTradePrice("16678291189211314787145083999015737376658799626183230671758641503291735614088", "buy");
+    const resp = await clobClient.getLastTradePrice("1343197538147866997676250008839231694243646439454152539053893078719042421992");
     console.log(resp);
 }
 
