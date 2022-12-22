dd9b1628eee62a8194fe4980a83745b72b632c4f
Rodrigo <95635797+poly-rodr@users.noreply.github.com>
2022-12-22 19:51:15 -0300

    Fixing js rounding
    A temp fix to avoid js rounding issues

--- src/order-builder/helpers.ts
+++ src/order-builder/helpers.ts
@@ -53,7 +53,7 @@ export const buildOrderCreationArgs = async (
         // force 2 decimals places
         const rawTakerAmt = roundDown(userOrder.size, 2);
         const rawPrice = roundNormal(userOrder.price, 2); // prob can just round this normal
-        const rawMakerAmt = roundDown(rawTakerAmt * rawPrice, 4);
+        const rawMakerAmt = roundDown(roundNormal(rawTakerAmt * rawPrice, 8), 4);
 
         makerAmount = parseUnits(rawMakerAmt.toString(), COLLATERAL_TOKEN_DECIMALS).toString();
         takerAmount = parseUnits(rawTakerAmt.toString(), CONDITIONAL_TOKEN_DECIMALS).toString();
@@ -62,7 +62,7 @@ export const buildOrderCreationArgs = async (
 
         const rawMakerAmt = roundDown(userOrder.size, 2);
         const rawPrice = roundNormal(userOrder.price, 2);
-        const rawTakerAmt = roundDown(rawPrice * rawMakerAmt, 4);
+        const rawTakerAmt = roundDown(roundNormal(rawPrice * rawMakerAmt, 8), 4);
 
         makerAmount = parseUnits(rawMakerAmt.toString(), CONDITIONAL_TOKEN_DECIMALS).toString();
         takerAmount = parseUnits(rawTakerAmt.toString(), COLLATERAL_TOKEN_DECIMALS).toString();
@@ -138,7 +138,7 @@ export const buildMarketBuyOrderCreationArgs = async (
     // force 2 decimals places
     const rawMakerAmt = roundDown(userMarketOrder.amount, 2);
     const rawPrice = roundDown(userMarketOrder.price || 1, 2);
-    const rawTakerAmt = roundDown(rawMakerAmt / rawPrice, 4);
+    const rawTakerAmt = roundDown(roundNormal(rawMakerAmt / rawPrice, 8), 4);
 
     const makerAmount = parseUnits(rawMakerAmt.toString(), COLLATERAL_TOKEN_DECIMALS).toString();
     const takerAmount = parseUnits(rawTakerAmt.toString(), CONDITIONAL_TOKEN_DECIMALS).toString();
