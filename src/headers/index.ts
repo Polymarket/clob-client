22a136e5bc4f56c6a105b432b18c60c9a454d4a3
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2025-09-26 17:32:32 -0400

    fix: include builder auth into postOrder flows

--- src/headers/index.ts
+++ src/headers/index.ts
@@ -1,7 +1,8 @@
 import { JsonRpcSigner } from "@ethersproject/providers";
 import { Wallet } from "@ethersproject/wallet";
 import { buildClobEip712Signature, buildPolyHmacSignature } from "../signing";
-import { ApiKeyCreds, Chain, L1PolyHeader, L2HeaderArgs, L2PolyHeader } from "../types";
+import { ApiKeyCreds, Chain, L1PolyHeader, L2HeaderArgs, L2PolyHeader, L2WithBuilderHeader } from "../types";
+import { BuilderApiKeyCreds, BuilderHeaderPayload, BuilderSigner } from "@polymarket/builder-signing-sdk";
 
 export const createL1Headers = async (
     signer: Wallet | JsonRpcSigner,
@@ -60,3 +61,42 @@ export const createL2Headers = async (
 
     return headers;
 };
+
+export const createBuilderHeaders = async (
+    builderCreds: BuilderApiKeyCreds,
+    l2Header: L2PolyHeader,
+    l2HeaderArgs: L2HeaderArgs,
+    timestamp?: number,
+): Promise<L2WithBuilderHeader> => {
+    let ts = Math.floor(Date.now() / 1000);
+    if (timestamp !== undefined) {
+        ts = timestamp;
+    }
+
+    const signer = new BuilderSigner(builderCreds);
+    const builderHeaders = signer.createBuilderHeaderPayload(
+        l2HeaderArgs.method,
+        l2HeaderArgs.requestPath,
+        l2HeaderArgs.body,
+    );
+
+    return injectBuilderHeaders(l2Header, builderHeaders);
+};
+
+export const injectBuilderHeaders = (
+    l2Header: L2PolyHeader,
+    builderHeaders: BuilderHeaderPayload,
+): L2WithBuilderHeader => {
+    return {
+        POLY_ADDRESS: l2Header.POLY_ADDRESS,
+        POLY_SIGNATURE: l2Header.POLY_SIGNATURE,
+        POLY_TIMESTAMP: l2Header.POLY_TIMESTAMP,
+        POLY_API_KEY: l2Header.POLY_API_KEY,
+        POLY_PASSPHRASE: l2Header.POLY_PASSPHRASE,
+        POLY_BUILDER_API_KEY: builderHeaders.POLY_BUILDER_API_KEY,
+        POLY_BUILDER_PASSPHRASE: builderHeaders.POLY_BUILDER_PASSPHRASE,
+        POLY_BUILDER_TIMESTAMP: builderHeaders.POLY_BUILDER_TIMESTAMP,
+        POLY_BUILDER_SIGNATURE: builderHeaders.POLY_BUILDER_SIGNATURE,
+    };
+}
+
--- src/types.ts
+++ src/types.ts
@@ -36,6 +36,20 @@ export interface L2PolyHeader extends AxiosRequestHeaders {
     POLY_PASSPHRASE: string;
 }
 
+
+// Builder API key verification
+export interface L2WithBuilderHeader extends AxiosRequestHeaders {
+    POLY_ADDRESS: string;
+    POLY_SIGNATURE: string;
+    POLY_TIMESTAMP: string;
+    POLY_API_KEY: string;
+    POLY_PASSPHRASE: string;
+    POLY_BUILDER_API_KEY: string;
+    POLY_BUILDER_TIMESTAMP: string;
+    POLY_BUILDER_PASSPHRASE: string;
+    POLY_BUILDER_SIGNATURE: string;
+}
+
 export enum Side {
     BUY = "BUY",
     SELL = "SELL",
