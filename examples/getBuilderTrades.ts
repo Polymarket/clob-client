a1fc654318d0e65d28ef428a32c13d88293aa8c3
JonathanAmenechi <7217608+JonathanAmenechi@users.noreply.github.com>
2025-10-03 12:21:50 -0400

    chore: standardize example

--- examples/getBuilderTrades.ts
+++ examples/getBuilderTrades.ts
@@ -9,14 +9,14 @@ dotenvConfig({ path: resolve(__dirname, "../.env") });
 
 async function main() {
     const wallet = new ethers.Wallet(`${process.env.PK}`);
-    const chainId = parseInt(`${process.env.CHAIN_ID_PROD || Chain.AMOY}`) as Chain;
+    const chainId = parseInt(`${process.env.CHAIN_ID || Chain.AMOY}`) as Chain;
     console.log(`Address: ${await wallet.getAddress()}, chainId: ${chainId}`);
 
-    const host = process.env.CLOB_API_URL_PROD || "http://localhost:8080";
+    const host = process.env.CLOB_API_URL || "http://localhost:8080";
     const creds: ApiKeyCreds = {
-        key: `${process.env.CLOB_API_KEY_PROD}`,
-        secret: `${process.env.CLOB_SECRET_PROD}`,
-        passphrase: `${process.env.CLOB_PASS_PHRASE_PROD}`,
+        key: `${process.env.CLOB_API_KEY}`,
+        secret: `${process.env.CLOB_SECRET}`,
+        passphrase: `${process.env.CLOB_PASS_PHRASE}`,
     };
 
     const builderCreds: BuilderApiKeyCreds = {
