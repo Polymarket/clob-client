ba8110ea0122d20f96491c96ec403f3569398f53
sahilgat02 <sahil@polymarket.com>
2025-12-30 21:15:24 +0800

    fix

--- examples/getOrderbook.ts
+++ examples/getOrderbook.ts
@@ -8,12 +8,12 @@ async function main() {
     const host = process.env.CLOB_API_URL || "http://localhost:8080";
     const chainId = parseInt(`${process.env.CHAIN_ID || Chain.AMOY}`) as Chain;
     const clobClient = new ClobClient(host, chainId);
-    const YES = "71321045679252212594626385532706912750332728571942532289631379312455583992563";
+    const YES = "34097058504275310827233323421517291090691602969494795225921954353603704046623";
 
     const orderbook = await clobClient.getOrderBook(YES);
     console.log("orderbook", orderbook);
 
-    const hash = clobClient.getOrderBookHash(orderbook);
+    const hash = await clobClient.getOrderBookHash(orderbook);
     console.log("orderbook hash", hash);
 }
 
