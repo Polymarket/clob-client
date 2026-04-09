ab376cde87411a6b8f943005cc162de4f8d4230f
Arun Shekhar <arun@polymarket.com>
2026-04-09 16:24:25 +0530

    deposit wallet init

--- src/order-utils/model/signature-types.model.ts
+++ src/order-utils/model/signature-types.model.ts
@@ -13,4 +13,10 @@ export enum SignatureType {
      * EIP712 signatures signed by EOAs that own Polymarket Gnosis safes
      */
     POLY_GNOSIS_SAFE = 2,
+
+    /**
+     * EIP712 signatures signed by EOAs that own Polymarket Deposit Wallets.
+     * Validated on-chain via ERC-1271 isValidSignature() with ERC-7739 nested domain wrapping.
+     */
+    POLY_DEPOSIT_WALLET = 3,
 }
